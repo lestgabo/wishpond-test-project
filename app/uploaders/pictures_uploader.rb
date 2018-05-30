bb1a6d4f86f8aa52a0ce758d0a76554d5514acc9
lestgabo <lestgabo@gmail.com>
2018-05-30 00:54:02 +0000

    images wont show up

--- app/uploaders/pictures_uploader.rb
+++ app/uploaders/pictures_uploader.rb
@@ -1,8 +1,11 @@
 class PicturesUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  #include CarrierWave::MiniMagick
   # process resize_to_limit: [400, 400]
+    
+  # Process files as they are uploaded:
+  #process resize_to_fit: [100, 100]
   
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
@@ -31,8 +34,7 @@ class PicturesUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
+
   #
   # def scale(width, height)
   #   # do something
